e548d100d02078738d981b3ea1a778f9edd73b55
renzhifan <zhifan6797@163.com>
2019-04-11 18:03:22 +0800

    测试工作流

---
+++ tests/Unit/TeamTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Team;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TeamTest extends TestCase
+{
+    /** @test */
+    public function a_team_has_a_name()
+    {
+        $team = new Team(['name' => 'Acme']);
+
+        $this->assertEquals('Acme',$team->name);
+    }
+    /** @test */
+    public function a_team_can_add_members()
+    {
+        $team = factory('App\Team')->create();
+
+        $user = factory('App\User')->create();
+        $userTwo = factory('App\User')->create();
+
+        $team->add($user);
+        $team->add($userTwo);
+
+        $this->assertEquals(2,$team->count());
+    }
+    public function a_team_has_a_maximum_size()
+    {
+        $team = factory('App\Team')->create(['size' => 2]);
+
+        $user = factory('App\User')->create();
+        $userTwo = factory('App\User')->create();
+
+        $team->add($user);
+        $team->add($userTwo);
+
+        $this->assertEquals(2,$team->count());
+
+        $this->expectException('Exception');
+
+        $userThree = factory('App\User')->create();
+
+        $team->add($userThree);
+    }
+
+    public function a_team_can_add_multiple_members_at_once()
+    {
+        $team = factory('App\Team')->create();
+
+        $users = factory('App\User',2)->create();
+
+        $team->add($users);
+
+        $this->assertEquals(2,$team->count());
+    }
+}
